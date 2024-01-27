4f3fcc3db5841ba9a03b32050351236ca495dd3f
fshadrin <f.shadrin@velvetech.dev>
2024-01-27 16:50:07 +0700

    feat: created layout for app

---
+++ src/components/AppAside.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <aside class="w-1/5">
+    <h1>Todo planner</h1>
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </aside>
+</template>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppAside from './AppAside.vue'
+</script>
+<template>
+  <div class="flex h-dvh">
+    <AppAside />
+    <div class="flex-grow">
+      <header>header</header>
+      <main><slot /></main>
+    </div>
+  </div>
+</template>
